6a22cb430a4fba3e7ebd219106f38be90846a774
Your Name <leandro.reis.dev22@gmail.com>
2023-01-02 21:44:24 -0300

    Animação com CSS

---
+++ rotate.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>loading animation</title>
+</head>
+<link rel="stylesheet" type="text/css" href="rotate.css">
+<link rel="stylesheet" type="text/css" href="normalize.css">
+<body>
+<div class="topo">
+<div class="logo">
+    <img src= "cyber2.jpg">
+    <p>WEB<br>
+    DEVELOPER</p>
+</div>
+<div class="login">
+    <form>
+            <input type="email" nome="" class="email" placeholder= "Seu e-mail">
+            <input type="password" nome="" class="senha" placeholder= "Sua senha">
+            <input type="submit" nome="" value= "Logar" class="botao">
+    </form>
+</div>    
+
+<div class="banner">
+    <img src="cyber7.jpg">
+</div>
+
+<div class="animation is-animation"></div>
+<div class="animation1 is-animation1"> </div>
+<div class="animation2 is-animation2"></div>
+</div>
+
+
+
+
+<div class="menu"></div>
+
+<div class="P1">
+    <img src="cyber8.jpg"> 
+</div>
+<div class="P2"></div>
+<div class="P3"></div>
+<div class="P4"></div>
+
+</body>
+</html>
